9b8b2a8b60eae927df14545282a503dd4cab7042
SvLowman <SvenLohmann@gmx.de>
2021-02-23 10:42:07 +0100

    :sparkles: Build form for search

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import "./App.css";
 function App() {
   return (
     <main>
-      <p>...!</p>
+      <h1>Autocompletion Search</h1>
+      <form>
+        <label>Suche nach einem Ort:</label>
+        <input type="text" id="search_input" placeholder="Ort eingeben" />
+      </form>
     </main>
   );
 }
